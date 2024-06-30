4d6e0ac3bc373dc3add63a3ca9d12584d30f47cf
Davide Laneri <davidelan@hotmail.com>
2024-06-30 17:37:28 +0000

    Set debug off for deployment in heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-davidelan-djangoblogpro-vnhdjcxpe61.ws.codeinstitute-ide.net",
