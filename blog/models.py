e4a8e061f31cf1b86ef49207a54f3991dffa9582
Davide Laneri <davidelan@hotmail.com>
2024-06-30 16:08:05 +0000

    Add urls.py in blog

--- blog/models.py
+++ blog/models.py
@@ -11,10 +11,16 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
     
 
 class Comment(models.Model):
@@ -25,3 +31,9 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
