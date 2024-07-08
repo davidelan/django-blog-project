804f41837929076cdd032e4444430a15e77166ef
Davide Laneri <davidelan@hotmail.com>
2024-07-08 19:10:27 +0000

    Tidy up code

--- about/views.py
+++ about/views.py
@@ -6,7 +6,17 @@ from .forms import CollaborateForm
 
 def about_me(request):
     """
-    Renders the About page
+    Renders the most recent information on the website author
+    and allows user colaboration requests
+    Displays an individual instance of :model:`about.About`.
+    **Context**
+    ``about``
+        The most recent instance of :model:`about.About`.
+    ``collaborate_Form``
+        An instance of :form:`about.CollaborateForm`.
+    **Template**
+    :template:`about/about.htmlAbout`.
+
     """
 
     if request.method == "POST":
