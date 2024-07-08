831955258d8d0e4659e6ac5536b5a3be744fed2f
Davide Laneri <davidelan@hotmail.com>
2024-07-08 13:55:15 +0000

    Correct admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -10,6 +10,7 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+    
 
 # Register your models here.
 admin.site.register(Comment)
