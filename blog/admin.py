28740b55cfa1796f290dcc1141f09aa237c34b25
Davide Laneri <davidelan@hotmail.com>
2024-07-08 15:22:35 +0000

    Eenable serving of image files

--- blog/admin.py
+++ blog/admin.py
@@ -10,7 +10,16 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on',)
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
-    
 
 # Register your models here.
 admin.site.register(Comment)
+
+#@admin.register(Comment)
+#class CommentAdmin(admin.ModelAdmin):
+#    #list_display = ('title', 'body', 'post', 'created_on', 'approved')
+#    list_filter = ('approved', 'created_on')
+#    search_fields = ('name', 'email', 'body')
+#    actions = ['approve_comments']
+
+#    def approve_comments(self, request, queryset):
+#        queryset.update(approved=True)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
@@ -14,6 +15,7 @@ class Post(models.Model):
     def __str__(self):
         return f"{self.title} | written by {self.author}"
 
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
